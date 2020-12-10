24f664e6f44049dbfa78f0917d1452647f49c8a4
Ramón Navarro <rnavarro@maubic.es>
2020-12-10 20:05:46 +0200

    second commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ImageA from './../images/productImage1500x500.jpeg'
 
 
-export default function Card() {
+export default function Card({target, name, price}) {
     return (
         <div className="cardContainer">
             <img
@@ -10,7 +10,9 @@ export default function Card() {
             src={ImageA}
             alt="First slide"
             ></img>
-            
+            <div className="targetCard">{target}</div>
+            <div className="nameCard">{name}</div>
+            <div className="priceCard">{price}</div>
         </div>
     )
 }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import Card from './Card'
 
+const productArray = [{target:"MAN", name: "magnifiseur", price: "€70.02"}, {target:"MAN", name: "magnifiseur", price: "€70.02"}, {target:"MAN", name: "magnifiseur", price: "€70.02"}, {target:"MAN", name: "magnifiseur", price: "€70.02"}, {target:"MAN", name: "magnifiseur", price: "€70.02"}]
+
 export default function ProductList() {
     return (
         <div className="productListContainer">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            {productArray.map(product=>(<Card target={product.target} name={product.name} price={product.price}/>))}
             
         </div>
     )
